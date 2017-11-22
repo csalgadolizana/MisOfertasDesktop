8a78067f68c4723befbc7c4546a8a8312b073274
felipedbs <pipe.dbs@gmail.com>
2017-11-22 15:42:19 -0300

    program

--- Program.cs
+++ Program.cs
@@ -18,6 +18,7 @@ namespace Desk
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             //Application.Run(new FrLogin());
+
             Application.Run(new FrLogin());
         }
     }
